99231907aed67779aef60f0628cde3ff5c774ea3
Ant King <antking1109@gmail.com>
2018-08-28 23:12:08 +0700

    create datable and create auth

---
+++ database/migrations/2018_08_28_143809_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title')->charset('utf8')->collation('utf8_unicode_ci');
+            $table->text('des')->charset('utf8')->collation('utf8_unicode_ci');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
