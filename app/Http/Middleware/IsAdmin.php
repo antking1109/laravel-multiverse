4b618114790a34073a54a833bfb315c70fa8cbe4
Ant King <antking1109@gmail.com>
2018-08-29 22:16:01 +0700

    Tạo middleware kiểm tra quyền admin

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()&&Auth::user()->level==1)
+        {
+            return $next($request);
+        }else
+        {
+            return redirect('/');
+        }
+    }
+}
